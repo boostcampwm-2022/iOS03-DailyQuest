959230adcc783f22ddd22f0faa969f3c9ba33658
jjinwoong <baseball0020@gmail.com>
2022-11-11 11:38:12 +0900

    [chore] Add git ignore file.

---
+++ DailyQuest/DailyQuest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyQuest
+//
+//  Created by jinwoong Kim on 2022/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
