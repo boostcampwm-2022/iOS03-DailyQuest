5e801f99e277beeb3357cf96489d0325f0c24de7
jjinwoong <baseball0020@gmail.com>
2022-11-14 15:39:27 +0900

    [feat] 퀘스트와 유저 엔티티 추가

---
+++ DailyQuest/DailyQuest/Domain/Entities/Quest.swift
@@ -0,0 +1,37 @@
+//
+//  Quest.swift
+//  DailyQuest
+//
+//  Created by jinwoong Kim on 2022/11/14.
+//
+
+import Foundation
+
+struct Quest {
+    let title: String
+    let startDay: Date
+    let endDay: Date
+    let `repeat`: Int
+    var currentCount: Int
+    let totalCount: Int
+    
+    var state: Bool {
+        return currentCount == totalCount
+    }
+    
+    mutating func increaseCount(with value: Int=1) {
+        guard currentCount + value <= totalCount else {
+            self.currentCount = totalCount
+            return
+        }
+        self.currentCount += value
+    }
+    
+    mutating func decreaseCount(with value: Int=1) {
+        guard currentCount - value >= 0 else {
+            self.currentCount = 0
+            return
+        }
+        self.currentCount -= value
+    }
+}
---
+++ DailyQuest/DailyQuest/Domain/Entities/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  DailyQuest
+//
+//  Created by jinwoong Kim on 2022/11/14.
+//
+
+import Foundation
+
+struct User {
+    let uuid: UUID
+    let nickName: String
+    let profile: Data
+    let backgroundImage: Data
+    let description: String
+}
