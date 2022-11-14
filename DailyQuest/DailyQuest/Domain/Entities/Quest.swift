cdb2dafeee240a4e5a3ec190efa3116da8671876
jjinwoong <baseball0020@gmail.com>
2022-11-14 18:00:43 +0900

    [style] Quest entity에 주석 추가.

--- DailyQuest/DailyQuest/Domain/Entities/Quest.swift
+++ DailyQuest/DailyQuest/Domain/Entities/Quest.swift
@@ -19,6 +19,13 @@ struct Quest {
         return currentCount == totalCount
     }
     
+    /**
+     현재 목표달성량(currentCount)에 인자값만큼 더합니다.
+     Note. 현재 목표달성량은 전체량(totalCount)를 넘을 수 없습니다.
+     
+     - Parameters:
+        - value: 0보다 큰 정수값입니다. 기본값은 1입니다.
+     */
     mutating func increaseCount(with value: Int=1) {
         guard currentCount + value <= totalCount else {
             self.currentCount = totalCount
@@ -27,6 +34,13 @@ struct Quest {
         self.currentCount += value
     }
     
+    /**
+     현재 목표달성량(currentCount)에 인자값만큼 뺍니다.
+     Note. 현재 목표달성량은 0보다 작을 수 없습니다.
+     
+     - Parameters:
+        - value: 0보다 큰 정수값입니다. 기본값은 1입니다.
+     */
     mutating func decreaseCount(with value: Int=1) {
         guard currentCount - value >= 0 else {
             self.currentCount = 0
