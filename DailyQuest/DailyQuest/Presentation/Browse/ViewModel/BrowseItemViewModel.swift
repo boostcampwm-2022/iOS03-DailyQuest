41ca5a696e396f2fe3e6f4460f8d1ae37d00ea00
jjinwoong <baseball0020@gmail.com>
2022-11-15 16:36:13 +0900

    [feat] 가짜 데이터를 생성하는 viewmodel 작성

--- DailyQuest/DailyQuest/Presentation/Browse/ViewModel/BrowseItemViewModel.swift
+++ DailyQuest/DailyQuest/Presentation/Browse/ViewModel/BrowseItemViewModel.swift
@@ -1,8 +1,18 @@
 //
-//  BrowseCellViewModel.swift
+//  BrowseCellItemModel.swift
 //  DailyQuest
 //
 //  Created by jinwoong Kim on 2022/11/15.
 //
 
 import Foundation
+
+final class BrowseItemViewModel {
+    let user: User
+    let quests: [Quest]
+    
+    init(user: User, quests: [Quest]) {
+        self.user = user
+        self.quests = quests
+    }
+}
--- DailyQuest/DailyQuest/Presentation/Browse/ViewModel/BrowseViewModel.swift
+++ DailyQuest/DailyQuest/Presentation/Browse/ViewModel/BrowseViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+import RxSwift
+
+final class BrowseViewModel {
+    let user = User(uuid: UUID(), nickName: "jinwoong", profile: Data(), backgroundImage: Data(), description: "")
+    let quests = [
+        Quest(title: "물마시기", startDay: Date(), endDay: Date(), repeat: 1, currentCount: 2, totalCount: 5),
+        Quest(title: "코딩하기", startDay: Date(), endDay: Date(), repeat: 1, currentCount: 0, totalCount: 10)
+    ]
+    let data: Observable<[(User, [Quest])]>
+    
+    let cellCount = [2]
+    
+    init() {
+        self.data = .just([(user, quests)])
+    }
+}
