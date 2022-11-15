6bc26556c531a9faca30dc0b970a2526e4de40fd
Jeonhui <dlwjsgml02@naver.com>
2022-11-15 12:30:05 +0900

    [style] add final keyword (UserQuestEntity))

--- DailyQuest/DailyQuest/Data/PersistentStorages/RealmStorage/Entities/UserQuestEntity.swift
+++ DailyQuest/DailyQuest/Data/PersistentStorages/RealmStorage/Entities/UserQuestEntity.swift
@@ -8,16 +8,16 @@
 import Foundation
 import RealmSwift
 
-class UserQuestEntity: Object {
+final class UserQuestEntity: Object {
     @Persisted var title: String
     @Persisted var startDay: Date
     @Persisted var endDay: Date
     @Persisted var `repeat`: Int
     @Persisted var currentCount: Int
     @Persisted var totalCount: Int
-    
-    override init(){ }
-    
+
+    override init() { }
+
     init(title: String, startDay: Date, endDay: Date, currentCount: Int, totalCount: Int) {
         self.title = title
         self.startDay = startDay
