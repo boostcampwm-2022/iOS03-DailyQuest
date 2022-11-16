2ec6465c273258b3050c8653fb718dac6c79bb0a
jjinwoong <baseball0020@gmail.com>
2022-11-16 11:45:19 +0900

    [style] Scene Delegate 수정

--- DailyQuest/DailyQuest/Application/SceneDelegate.swift
+++ DailyQuest/DailyQuest/Application/SceneDelegate.swift
@@ -10,17 +10,23 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var appCoordinator: AppCoordinator?
+    let appDIContainer = AppDIContainer()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let cont = BrowseViewController.create(with: BrowseViewModel())
+        let tabbarController = UITabBarController()
+        
         self.window = UIWindow(windowScene: windowScene)
-        self.window?.rootViewController = cont
+        self.window?.rootViewController = tabbarController
         self.window?.makeKeyAndVisible()
+        
+        self.appCoordinator = AppCoordinator(tabBarController: tabbarController,
+                                             appDIContainer: appDIContainer)
+        self.appCoordinator?.start()
         
         return
     }
