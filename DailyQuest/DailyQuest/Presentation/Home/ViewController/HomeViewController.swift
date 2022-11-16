ed53e9251b5f7c06e9226657e582645022d8c1e3
jjinwoong <baseball0020@gmail.com>
2022-11-16 11:45:07 +0900

    [feat] Home View Controller 추가
    - 현재는 ViewModel를 가지고 있지는 않습니다. 추후에 추가합시다 :)

--- DailyQuest/DailyQuest/Presentation/Home/ViewController/HomeViewController.swift
+++ DailyQuest/DailyQuest/Presentation/Home/ViewController/HomeViewController.swift
@@ -5,4 +5,19 @@
 //  Created by jinwoong Kim on 2022/11/16.
 //
 
-import Foundation
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    
+    // MARK: - Life Cycle
+    // static func create(with viewModel: HomeViewModel)
+    static func create() -> HomeViewController {
+        return HomeViewController()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
