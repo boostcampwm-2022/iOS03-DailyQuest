646863a07e01adeab19a0e18d125a5066d7963f5
jjinwoong <baseball0020@gmail.com>
2022-11-16 22:49:28 +0900

    [feat] QuestViewHeader 구현.

--- DailyQuest/DailyQuest/Presentation/Home/View/QuestViewHeader.swift
+++ DailyQuest/DailyQuest/Presentation/Home/View/QuestViewHeader.swift
@@ -5,4 +5,57 @@
 //  Created by jinwoong Kim on 2022/11/16.
 //
 
-import Foundation
+import UIKit
+
+import RxSwift
+import RxCocoa
+import SnapKit
+
+final class QuestViewHeader: UIStackView {
+    private var disposableBag = DisposeBag()
+    var buttonDidClick = PublishSubject<Void>()
+    
+    // MARK: - Components
+    private lazy var titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.text = "Today Quest"
+        titleLabel.textColor = .maxViolet
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 32)
+        
+        return titleLabel
+    }()
+    
+    private lazy var plusButton: UIButton = {
+        var config = UIButton.Configuration.maxStyle()
+        let plusButton = UIButton(configuration: config)
+                
+        return plusButton
+    }()
+    
+    // MARK: - Methods
+    convenience init() {
+        self.init(frame: .zero)
+        
+        axis = .horizontal
+        alignment = .center
+        distribution = .equalSpacing
+        isLayoutMarginsRelativeArrangement = true
+        layoutMargins = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        
+        configureUI()
+        bind()
+    }
+    
+    private func configureUI() {
+        addArrangedSubview(titleLabel)
+        addArrangedSubview(plusButton)
+    }
+    
+    private func bind() {
+        plusButton.rx.tap
+            .subscribe(onNext: { [weak self] _ in
+                self?.buttonDidClick.onNext(())
+            })
+            .disposed(by: disposableBag)
+    }
+}
