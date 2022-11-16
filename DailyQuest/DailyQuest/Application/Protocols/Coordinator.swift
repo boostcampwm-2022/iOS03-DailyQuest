d673792fa7083157d1714f54f6993d4fef12eec2
jjinwoong <baseball0020@gmail.com>
2022-11-16 11:43:47 +0900

    [feat] Coordinator 프로토콜 작성

--- DailyQuest/DailyQuest/Application/Protocols/Coordinator.swift
+++ DailyQuest/DailyQuest/Application/Protocols/Coordinator.swift
@@ -5,4 +5,23 @@
 //  Created by jinwoong Kim on 2022/11/16.
 //
 
-import Foundation
+import UIKit
+
+protocol Coordinator: AnyObject {
+    var finishDelegate: CoordinatorFinishDelegate? { get set }
+    var childCoordinators: [Coordinator] { get set }
+    
+    func start()
+    func finish()
+}
+
+extension Coordinator {
+    func finish() {
+        childCoordinators.removeAll()
+        finishDelegate?.coordinatorDidFinish(childCoordinator: self)
+    }
+}
+
+protocol CoordinatorFinishDelegate: AnyObject {
+    func coordinatorDidFinish(childCoordinator: Coordinator)
+}
