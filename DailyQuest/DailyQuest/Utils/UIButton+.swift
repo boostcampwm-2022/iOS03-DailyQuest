cac964e3b4e0036c64c1c7a35b179e89d40b5421
jjinwoong <baseball0020@gmail.com>
2022-11-16 22:50:06 +0900

    [feat] PlusButton 스타일 추가.

--- DailyQuest/DailyQuest/Utils/UIButton+.swift
+++ DailyQuest/DailyQuest/Utils/UIButton+.swift
@@ -5,4 +5,16 @@
 //  Created by jinwoong Kim on 2022/11/16.
 //
 
-import Foundation
+import UIKit
+
+extension UIButton.Configuration {
+    public static func maxStyle() -> UIButton.Configuration {
+        var style = UIButton.Configuration.plain()
+        style.image = UIImage(systemName: "plus")
+        style.baseForegroundColor = .maxViolet
+        style.background.backgroundColor = .maxLightYellow
+        style.cornerStyle = .capsule
+        
+        return style
+    }
+}
