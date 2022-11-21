8efb04b06b21e9311bbcede17cc2a47cc5c3c852
jjinwoong <baseball0020@gmail.com>
2022-11-21 19:02:36 +0900

    [feat] QuestViewModel 수정

--- DailyQuest/DailyQuest/Presentation/Home/ViewModel/QuestViewModel.swift
+++ DailyQuest/DailyQuest/Presentation/Home/ViewModel/QuestViewModel.swift
@@ -8,18 +8,32 @@
 import Foundation
 
 import RxSwift
+import RxCocoa
 
 final class QuestViewModel {
     private let questUseCase: QuestUseCase
-    
-//    let quests = [
-//        Quest(groupId: UUID(), uuid: UUID(), title: "물마시기", currentCount: 4, totalCount: 5),
-//        Quest(groupId: UUID(), uuid: UUID(), title: "책읽기", currentCount: 9, totalCount: 20),
-//        Quest(groupId: UUID(), uuid: UUID(), title: "달리기", currentCount: 4, totalCount: 9),
-//        Quest(groupId: UUID(), uuid: UUID(), title: "잠자기", currentCount: 1, totalCount: 1)
-//    ]
-    
+        
     init(questUseCase: QuestUseCase) {
         self.questUseCase = questUseCase
+    }
+    
+    struct Input {
+        let viewDidLoad: Observable<Date>
+    }
+    
+    struct Output {
+        let data = PublishSubject<[Quest]>()
+    }
+    
+    func transform(input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+        
+        input
+            .viewDidLoad
+            .flatMap(questUseCase.fetch(by:))
+            .bind(to: output.data)
+            .disposed(by: disposeBag)
+        
+        return output
     }
 }
