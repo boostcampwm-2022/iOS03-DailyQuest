ae0590c60afd06ee4e547a5440099ddaecfeed26
jjinwoong <baseball0020@gmail.com>
2022-11-21 16:32:58 +0900

    [test] Quest Repository 목업 생성

--- DailyQuest/DailyQuestTests/Domain/UseCases/Mocks/QuestRepositoryMock.swift
+++ DailyQuest/DailyQuestTests/Domain/UseCases/Mocks/QuestRepositoryMock.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+import RxSwift
+
+final class QuestRepositoryMock: QuestsRepository {
+    let quests = [
+        Quest.stub(groupId: UUID(),
+                   uuid: UUID(),
+                   title: "물마시기",
+                   currentCount: 0,
+                   totalCount: 10),
+        Quest.stub(groupId: UUID(),
+                   uuid: UUID(),
+                   title: "물마시기",
+                   currentCount: 0,
+                   totalCount: 10),
+    ]
+    
+    func save(with quest: [Quest]) -> Single<[Quest]> {
+        
+        return Single.just([])
+    }
+
+    func update(with quest: Quest) -> Single<Quest> {
+        return Single.just(quests[0])
+    }
+    
+    func delete(with questId: UUID) -> Single<Quest> {
+        return Single.just(quests[0])
+    }
+    
+    func deleteAll(with groupId: UUID) -> Single<[Quest]> {
+        return Single.just(quests)
+    }
+    
+    func fetch(by date: Date) -> Observable<[Quest]> {
+        return Observable.just(quests)
+    }
+}
