2825a40553696fae77a7e0dd8c1bb46aff5ea64a
jjinwoong <baseball0020@gmail.com>
2022-11-21 19:03:28 +0900

    [test] QuestViewModel test 코드 작성
    - RxCocoa.runtime이 없다는 에러가 발생함.

--- DailyQuest/DailyQuestTests/Presentation/ViewModel/QuestViewModelTests.swift
+++ DailyQuest/DailyQuestTests/Presentation/ViewModel/QuestViewModelTests.swift
@@ -7,7 +7,12 @@
 
 import XCTest
 
+import RxSwift
+
 final class QuestViewModelTests: XCTestCase {
+    private var questViewModel: QuestViewModel!
+    private var questUseCase: QuestUseCase!
+    private var disposeBag = DisposeBag()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -15,14 +20,34 @@ final class QuestViewModelTests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        questViewModel = nil
+        questUseCase = nil
+        disposeBag = .init()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testQuestViewModel_WhenUseCaseSendCorrectQuests_ThenExpectationWillBeFulfilledWithSuccess() {
+        // given
+        questUseCase = QuestUseCaseMock()
+        
+        questViewModel = QuestViewModel(questUseCase: questUseCase)
+        
+        let expectation = XCTestExpectation(description: "test success")
+        
+        // when
+        let output = questViewModel.transform(input: QuestViewModel.Input(viewDidLoad: .just(Date())), disposeBag: disposeBag)
+        
+        // then
+        output
+            .data
+            .subscribe(onNext: { data in
+                print(data)
+                expectation.fulfill()
+            }, onError: { _ in
+                XCTFail("test failed")
+            })
+            .disposed(by: disposeBag)
+        
+        wait(for: [expectation], timeout: 1)
     }
 
     func testPerformanceExample() throws {
