7369f95fbcce82049c4478c49449780cf64b14b4
jjinwoong <baseball0020@gmail.com>
2022-11-22 14:07:39 +0900

    [feat] Status View 추가

--- DailyQuest/DailyQuest/Presentation/Home/View/StatusView.swift
+++ DailyQuest/DailyQuest/Presentation/Home/View/StatusView.swift
@@ -7,4 +7,19 @@
 
 import UIKit
 
-
+final class StatusView: UIView {
+    
+    private lazy var iconContainer: UIImageView = {
+        let iconContainer = UIImageView()
+        
+        return iconContainer
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
