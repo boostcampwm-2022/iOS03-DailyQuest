58226d3dc984a27112e6d52067fe2baf6067406d
Jeonhui <dlwjsgml02@naver.com>
2022-11-23 00:14:45 +0900

    [fix] Quest stub

--- DailyQuest/DailyQuestTests/Mocks/Quest+Stub.swift
+++ DailyQuest/DailyQuestTests/Mocks/Quest+Stub.swift
@@ -10,11 +10,13 @@ import Foundation
 extension Quest {
     static func stub(groupId: UUID,
                      uuid: UUID,
+                     date: Date,
                      title: String,
                      currentCount: Int,
                      totalCount: Int) -> Self {
         return .init(groupId: groupId,
                      uuid: uuid,
+                     date: date,
                      title: title,
                      currentCount: currentCount,
                      totalCount: totalCount)
