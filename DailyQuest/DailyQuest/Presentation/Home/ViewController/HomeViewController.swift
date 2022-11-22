4261c5b5e7ac71ae98f887362edfe48ab1660246
Dayeon Lee <dayeon2768@daum.net>
2022-11-22 13:24:37 +0900

    [feat] add animation complete

--- DailyQuest/DailyQuest/Presentation/Home/ViewController/HomeViewController.swift
+++ DailyQuest/DailyQuest/Presentation/Home/ViewController/HomeViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import RxSwift
 import RxCocoa
 import SnapKit
+import Lottie
 
 final class HomeViewController: UIViewController {
     enum Event {
@@ -32,6 +33,13 @@ final class HomeViewController: UIViewController {
         return QuestViewHeader()
     }()
     
+    private lazy var animationView: LottieAnimationView = {
+        let animView = LottieAnimationView(name: "max-loading")
+        animView.frame = CGRect(x:0,y:0,width: 400, height: 500)
+        animView.contentMode = .scaleAspectFill
+        return animView
+    }()
+    
     // MARK: - Life Cycle
     // static func create(with viewModel: HomeViewModel)
     static func create() -> HomeViewController {
@@ -40,18 +48,24 @@ final class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        questViewDelegate = QuestViewDelegate(header: questViewHeader)
-        
         view.backgroundColor = .white
-        
-        view.addSubview(questView)
-        
-        questView.delegate = questViewDelegate
-        questView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+        view.addSubview(animationView)
+        animationView.center = view.center
+        self.tabBarController?.tabBar.isHidden = true
+        self.tabBarController?.tabBar.isTranslucent = true
+        // 애니메이션 실행
+        animationView.play{ (finish) in
+            self.animationView.removeFromSuperview()
+            self.tabBarController?.tabBar.isHidden = false
+            self.questViewDelegate = QuestViewDelegate(header: self.questViewHeader)
+            self.view.backgroundColor = .white
+            self.view.addSubview(self.questView)
+            self.questView.delegate = self.questViewDelegate
+            self.questView.snp.makeConstraints { make in
+                make.edges.equalToSuperview()
+            }
+            self.bind()
         }
-        
-        bind()
     }
     
     private func bind() {
