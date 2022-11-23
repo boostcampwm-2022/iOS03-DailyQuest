bce8f70bd5940d4406853337c5e639352c72087a
jjinwoong <baseball0020@gmail.com>
2022-11-23 23:18:17 +0900

    [refactor] 피드백 반영
    - `fields`을 private let으로 변경, `register`를 해주는 private 메서드 추가

--- DailyQuest/DailyQuest/Presentation/Settings/ViewController/SettingsViewController.swift
+++ DailyQuest/DailyQuest/Presentation/Settings/ViewController/SettingsViewController.swift
@@ -9,25 +9,19 @@ import UIKit
 
 final class SettingsViewController: UITableViewController {
     
-    var fields: [CommonField] = [] {
-        didSet {
-            fields.forEach { field in
-                field.register(for: self.tableView)
-            }
-        }
-    }
+    private let fields: [CommonField]  = [
+        ToggleField(viewModel: .init(title: "둘러보기 허용", imageName: "person.crop.circle.badge.checkmark")),
+        PlainField(viewModel: .init(title: "some", info: "some...?", imageName: "pencil")),
+        NavigateField(viewModel: .init(title: "로그인", imageName: "person.circle.fill")),
+        PlainField(viewModel: .init(title: "앱 버전", info: "0.0.1", imageName: "exclamationmark.transmission"))
+    ]
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.separatorStyle = .singleLine
         
-        fields = [
-            ToggleField(viewModel: .init(title: "둘러보기 허용", imageName: "person.crop.circle.badge.checkmark")),
-            PlainField(viewModel: .init(title: "some", info: "some...?", imageName: "pencil")),
-            NavigateField(viewModel: .init(title: "로그인", imageName: "person.circle.fill")),
-            PlainField(viewModel: .init(title: "앱 버전", info: "0.0.1", imageName: "exclamationmark.transmission"))
-        ]
+        register()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -37,5 +31,11 @@ final class SettingsViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let field = fields[indexPath.row]
         return field.dequeue(for: tableView, at: indexPath)
+    }
+    
+    private func register() {
+        fields.forEach { field in
+            field.register(for: self.tableView)
+        }
     }
 }
