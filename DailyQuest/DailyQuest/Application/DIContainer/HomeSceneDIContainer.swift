4afe662caf41fb982ee8142bf830536ea4cd8562
jjinwoong <baseball0020@gmail.com>
2022-11-23 17:02:18 +0900

    [feat] DI 생성

--- DailyQuest/DailyQuest/Application/DIContainer/HomeSceneDIContainer.swift
+++ DailyQuest/DailyQuest/Application/DIContainer/HomeSceneDIContainer.swift
@@ -9,15 +9,26 @@ import UIKit
 
 final class HomeSceneDIContainer {
     
+    lazy var questsStorage: QuestsStorage = RealmQuestsStorage()
+    
     // MARK: - Repositories
+    func makeQuestsRepository() -> QuestsRepository {
+        return DefaultQuestsRepository(persistentStorage: questsStorage)
+    }
     
     // MARK: - Use Cases
+    func makeQuestUseCase() -> QuestUseCase {
+        return DefaultQuestUseCase(questsRepository: makeQuestsRepository())
+    }
     
     // MARK: - View Models
+    func makeQuestViewModel() -> QuestViewModel {
+        return QuestViewModel(questUseCase: makeQuestUseCase())
+    }
     
     // MARK: - View Controller
     func makeHomeViewController() -> HomeViewController {
-        return HomeViewController.create()
+        return HomeViewController.create(with: makeQuestViewModel())
     }
     
     // MARK: - Flow
