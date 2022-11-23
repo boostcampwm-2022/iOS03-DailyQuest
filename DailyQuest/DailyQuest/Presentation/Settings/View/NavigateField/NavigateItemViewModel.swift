210ab56ccdc85c47f1327a57ca853b3f0a5371ac
jjinwoong <baseball0020@gmail.com>
2022-11-23 19:40:17 +0900

    [style] 필요한 파일들 생성

---
+++ DailyQuest/DailyQuest/Presentation/Settings/View/NavigateField/NavigateItemViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NavigateItemViewModel.swift
+//  DailyQuest
+//
+//  Created by jinwoong Kim on 2022/11/23.
+//
+
+import Foundation
---
+++ DailyQuest/DailyQuest/Presentation/Settings/ViewController/SettingsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsViewController.swift
+//  DailyQuest
+//
+//  Created by jinwoong Kim on 2022/11/23.
+//
+
+import Foundation
