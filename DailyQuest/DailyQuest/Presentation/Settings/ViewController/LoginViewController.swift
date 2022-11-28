9b1cd825ee9fe2f84bd746f5f81ebb3aaf2f9c23
jjinwoong <baseball0020@gmail.com>
2022-11-28 18:50:50 +0900

    [style] #54 loginviewcontroller 파일 추가

---
+++ DailyQuest/DailyQuest/Presentation/Settings/ViewController/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  DailyQuest
+//
+//  Created by jinwoong Kim on 2022/11/28.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemCyan
+    }
+}
