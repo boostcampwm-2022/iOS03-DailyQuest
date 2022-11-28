156318fda0ceba29f0229da1cb18eda601207c35
jjinwoong <baseball0020@gmail.com>
2022-11-28 17:04:50 +0900

    [feat] AuthViewModel 작성

--- DailyQuest/DailyQuest/Presentation/Settings/ViewModel/AuthViewModel.swift
+++ DailyQuest/DailyQuest/Presentation/Settings/ViewModel/AuthViewModel.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+import RxSwift
+import RxCocoa
+
+final class AuthViewModel {
+    private let authUseCase: AuthUseCase
+    
+    init(authUseCase: AuthUseCase) {
+        self.authUseCase = authUseCase
+    }
+    
+    struct Input {
+        let emailFieldDidEditEvent: Observable<String>
+        let passwordFieldDidEditEvent: Observable<String>
+        let submitButtonDidTapEvent: Observable<Void>
+    }
+    
+    struct Output {
+        let buttonEnabled: Driver<Bool>
+        let loginResult: Observable<Bool>
+    }
+    
+    func transform(input: Input, disposeBag: DisposeBag) -> Output {
+        let buttonEnabled = Observable
+            .combineLatest(input.emailFieldDidEditEvent,
+                           input.passwordFieldDidEditEvent) { !$0.isEmpty && !$1.isEmpty }
+            .asDriver(onErrorJustReturn: false)
+        
+        let loginResult = input
+            .submitButtonDidTapEvent
+            .withLatestFrom(Observable
+                .combineLatest(input.emailFieldDidEditEvent,
+                               input.passwordFieldDidEditEvent))
+            .flatMap(authUseCase.signIn(email:password:))
+        
+        return Output(buttonEnabled: buttonEnabled, loginResult: loginResult)
+    }
+}
