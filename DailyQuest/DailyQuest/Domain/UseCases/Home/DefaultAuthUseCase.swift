c5cfa27b24966336332802debac41bee2320a714
jjinwoong <baseball0020@gmail.com>
2022-11-28 17:03:41 +0900

    [feat] DefaultAuthUseCase 추가

--- DailyQuest/DailyQuest/Domain/UseCases/Home/DefaultAuthUseCase.swift
+++ DailyQuest/DailyQuest/Domain/UseCases/Home/DefaultAuthUseCase.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+import RxSwift
+
+final class DefaultAuthUseCase {
+    private let authRepository: AuthRepository
+    
+    init(authRepository: AuthRepository) {
+        self.authRepository = authRepository
+    }
+}
+
+extension DefaultAuthUseCase: AuthUseCase {
+    func signIn(email: String, password: String) -> Observable<Bool> {
+        return authRepository
+            .signIn(email: email, password: password)
+            .catch { _ in
+                return .just(false)
+            }
+            .asObservable()
+    }
+    
+    func signOut() -> Observable<Bool> {
+        return authRepository
+            .signOut()
+            .catch { _ in
+                return .just(false)
+            }
+            .asObservable()
+    }
+}
