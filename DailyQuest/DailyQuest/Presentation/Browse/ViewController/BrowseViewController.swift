23919a07b5ead924e169fa3bc08fbd825e3e10d4
jjinwoong <baseball0020@gmail.com>
2022-11-29 17:14:10 +0900

    [feat] ViewController와 ViewModel을 bind

--- DailyQuest/DailyQuest/Presentation/Browse/ViewController/BrowseViewController.swift
+++ DailyQuest/DailyQuest/Presentation/Browse/ViewController/BrowseViewController.swift
@@ -43,7 +43,15 @@ final class BrowseViewController: UITableViewController {
     }
     
     private func bind() {
-
+        let output = viewModel.transform(input: BrowseViewModel.Input(viewDidLoad: .just(()).asObservable()))
+        
+        output
+            .data
+            .drive(tableView.rx.items(cellIdentifier: BrowseCell.reuseIdentifier, cellType: BrowseCell.self)) { row, item, cell in
+                cell.setup(with: item)
+            }
+            .disposed(by: disposableBag)
+            
     }
 }
 
@@ -54,15 +62,5 @@ extension BrowseViewController {
      */
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return (75.0 * CGFloat(viewModel.cellCount[indexPath.row])) + 20 + 75
-    }
-    
-    /**
-     viewModel에서 총 몇개의 BrowseCell을 만들어야하는지 결정합니다.
-     viewModel에는 UseCase를 통해 들어온 (User, [Quest]) 데이터에서 퀘스트의 **길이** 정보를 담는 프로퍼티(정ㅅ배열)가 있어야 합니다.
-     Note. 여기에서는 Quest 정보 자체가 필요하진 않습니다. 해당 배열의 아이템들은 위에서 사용할, BrowseCell의 크기를 결정하고,
-     배열 자체의 길이는 이 메서드의 결과를 결정합니다.
-     */
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.cellCount.count
     }
 }
