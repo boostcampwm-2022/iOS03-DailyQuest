524381b8a86e08d400bbfec493fb3466f6cb9e0a
jjinwoong <baseball0020@gmail.com>
2022-11-29 17:51:41 +0900

    [test] BrowseViewModel test

--- DailyQuest/DailyQuestTests/Presentation/ViewModel/BrowseViewModelTests.swift
+++ DailyQuest/DailyQuestTests/Presentation/ViewModel/BrowseViewModelTests.swift
@@ -7,7 +7,12 @@
 
 import XCTest
 
+import RxSwift
+
 final class BrowseViewModelTests: XCTestCase {
+    private var browseViewModel: BrowseViewModel!
+    private var browseUseCase: BrowseUseCase!
+    private var disposeBag = DisposeBag()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -15,14 +20,31 @@ final class BrowseViewModelTests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        browseViewModel = nil
+        browseUseCase = nil
+        disposeBag = .init()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testBrowseViewModel_WhenUseCaseSendTwoUsersQuests_ThenViewModelWillGenerateTwoBrowseItemViewModel() {
+        // given
+        browseUseCase = BrowseUseCaseMock()
+        
+        browseViewModel = BrowseViewModel(browseUseCase: browseUseCase)
+        
+        // when
+        let output = browseViewModel
+            .transform(
+                input: BrowseViewModel
+                    .Input(viewDidLoad: .just(()).asObservable())
+            )
+        
+        // then
+        output
+            .data
+            .drive(onNext: { items in
+                XCTAssertEqual(2, items.count)
+            })
+            .disposed(by: disposeBag)
     }
 
     func testPerformanceExample() throws {
