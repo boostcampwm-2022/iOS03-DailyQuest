f8a77f276a2dca3f78106f730c66941b50b8e447
jjinwoong <baseball0020@gmail.com>
2022-11-29 17:13:19 +0900

    [feat] BrowseViewModel 작성

--- DailyQuest/DailyQuest/Presentation/Browse/ViewModel/BrowseViewModel.swift
+++ DailyQuest/DailyQuest/Presentation/Browse/ViewModel/BrowseViewModel.swift
@@ -8,7 +8,44 @@
 import Foundation
 
 import RxSwift
+import RxCocoa
 
 final class BrowseViewModel {
-
+    private let browseUseCase: BrowseUseCase
+    private(set) var cellCount: [Int] = []
+    
+    init(browseUseCase: BrowseUseCase) {
+        self.browseUseCase = browseUseCase
+    }
+    
+    struct Input {
+        let viewDidLoad: Observable<Void>
+    }
+    
+    struct Output {
+        let data: Driver<[BrowseItemViewModel]>
+    }
+    
+    func transform(input: Input) -> Output {
+        let data = input
+            .viewDidLoad
+            .flatMap { _ in
+                self.browseUseCase.excute()
+            }
+            .map(transform(with:))
+            .do(onNext: { [weak self] items in
+                items.forEach { item in
+                    self?.cellCount.append(item.quests.count)
+                }
+            })
+            .asDriver(onErrorJustReturn: [])
+        
+        return Output(data: data)
+    }
+    
+    private func transform(with browseQuests: [BrowseQuest]) -> [BrowseItemViewModel] {
+        return browseQuests.map { browseQuest in
+            BrowseItemViewModel(user: browseQuest.user, quests: browseQuest.quests)
+        }
+    }
 }
