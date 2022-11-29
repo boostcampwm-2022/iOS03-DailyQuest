655e2cbe5ac191dca8e960d204936092591b12bc
jjinwoong <baseball0020@gmail.com>
2022-11-29 10:56:58 +0900

    [feat] BrowseRepo protocol 정의

---
+++ DailyQuest/DailyQuest/Domain/Interfaces/Repositories/BrowseRepository.swift
@@ -0,0 +1,15 @@
+//
+//  BrowseRepository.swift
+//  DailyQuest
+//
+//  Created by jinwoong Kim on 2022/11/29.
+//
+
+import Foundation
+
+import RxSwift
+
+protocol BrowseRepository {
+    // TODO: 무한 스크롤을 위한 페이징 추가
+    func fetch() -> Observable<[BrowseQuest]>
+}
