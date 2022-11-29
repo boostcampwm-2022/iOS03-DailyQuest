0495bef426cbb1194ca7ffba4474f4cf0f41f30f
jjinwoong <baseball0020@gmail.com>
2022-11-29 17:50:52 +0900

    [test] BrowseUseCase test

--- DailyQuest/DailyQuestTests/Domain/UseCases/BrowseUseCaseTests.swift
+++ DailyQuest/DailyQuestTests/Domain/UseCases/BrowseUseCaseTests.swift
@@ -7,7 +7,12 @@
 
 import XCTest
 
+import RxSwift
+
 final class BrowseUseCaseTests: XCTestCase {
+    private var browseUseCase: BrowseUseCase!
+    private var browseRepo: BrowseRepository!
+    private var disposeBag = DisposeBag()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -15,14 +20,32 @@ final class BrowseUseCaseTests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        browseRepo = nil
+        browseUseCase = nil
+        disposeBag = .init()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testBrowseUseCase_WhenRepoSendCorrectQuests_ThenExpectationWillFulfillWithSuccess() {
+        // given
+        browseRepo = BrowseRepositoryMock()
+        
+        browseUseCase = DefaultBrowseUseCase(browseRepository: browseRepo)
+        
+        let expectation = XCTestExpectation(description: "test success")
+        
+        // when
+        browseUseCase
+            .excute()
+        // then
+            .subscribe(onNext: { browseQuests in
+                print(browseQuests)
+                expectation.fulfill()
+            }, onError: { error in
+                print(error)
+            })
+            .disposed(by: disposeBag)
+        
+        wait(for: [expectation], timeout: 1)
     }
 
     func testPerformanceExample() throws {
