a69edfed93e870cf642fd35474139be8e053f137
jjinwoong <baseball0020@gmail.com>
2022-11-29 15:16:43 +0900

    [feat] Browse usecase 정의

---
+++ DailyQuest/DailyQuest/Domain/UseCases/Browse/DefaultBrowseUseCase.swift
@@ -0,0 +1,8 @@
+//
+//  DefaultBrowseUseCase.swift
+//  DailyQuest
+//
+//  Created by jinwoong Kim on 2022/11/29.
+//
+
+import Foundation
---
+++ DailyQuest/DailyQuest/Domain/UseCases/Browse/Protocols/BrowseUseCase.swift
@@ -0,0 +1,14 @@
+//
+//  BrowseUseCase.swift
+//  DailyQuest
+//
+//  Created by jinwoong Kim on 2022/11/29.
+//
+
+import Foundation
+
+import RxSwift
+
+protocol BrowseUseCase {
+    func excute() -> Observable<[BrowseQuest]>
+}
