3a78b8187b863a70fa6bd6dcc64c05384276c925
jjinwoong <baseball0020@gmail.com>
2022-11-30 22:14:04 +0900

    [feat] EnrollViewController의 서브뷰들을 관장하는 viewmodel 작성

--- DailyQuest/DailyQuest/Presentation/Home/ViewModel/EnrollViewModel.swift
+++ DailyQuest/DailyQuest/Presentation/Home/ViewModel/EnrollViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+import RxSwift
+
+final class EnrollViewModel {
+    let dayNamePickerViewModel: DayNamePickerViewModel
+    
+    init(dayNamePickerViewModel: DayNamePickerViewModel) {
+        self.dayNamePickerViewModel = dayNamePickerViewModel
+    }
+    
+    struct Input {
+        let titleDidChanged: Observable<String>
+        let startDateDidSet: Observable<Date>
+        let endDateDidSet: Observable<Date>
+        let quantityDidSet: Observable<Int>
+        
+        /** doneButtonDidClickedEvnet */
+    }
+    
+    struct Output {
+        let enrollResult: Observable<Bool>
+    }
+    
+    func transform(input: Input) {}
+}
