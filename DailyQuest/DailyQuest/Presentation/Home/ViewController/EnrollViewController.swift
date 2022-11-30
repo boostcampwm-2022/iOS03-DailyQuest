a0c51f3592263e3871b42d6b87e6a7d9d96148c2
jjinwoong <baseball0020@gmail.com>
2022-11-30 21:34:13 +0900

    [feat] Enroll View Controller 구성
    - 비즈니스 로직과 연결되어 있지 않음.

--- DailyQuest/DailyQuest/Presentation/Home/ViewController/EnrollViewController.swift
+++ DailyQuest/DailyQuest/Presentation/Home/ViewController/EnrollViewController.swift
@@ -7,30 +7,81 @@
 
 import UIKit
 
+import RxSwift
 import SnapKit
 
 final class EnrollViewController: UIViewController {
+    private var disposableBag = DisposeBag()
     
-    private lazy var indicateMessage: UILabel = {
-        let indicateMessage = UILabel()
-        indicateMessage.text = "여기서 새로운 퀘스트를 추가합니다."
+    private lazy var container: UIStackView = {
+        let container = UIStackView()
+        container.axis = .vertical
+        container.spacing = 10
         
-        return indicateMessage
+        return container
     }()
     
+    private lazy var titleField: TextFieldForm = {
+        let titleField = TextFieldForm()
+        titleField.placeholder = "description"
+        titleField.backgroundColor = .maxYellow
+        titleField.layer.cornerRadius = 15
+        
+        return titleField
+    }()
+    
+    private lazy var startDate: PlanDatePickerView = {
+        let startDate = PlanDatePickerView()
+        
+        return startDate
+    }()
+    
+    private lazy var endDate: PlanDatePickerView = {
+        let endDate = PlanDatePickerView()
+        
+        return endDate
+    }()
+    
+    private lazy var daysPicker: DayNamePickerView = {
+        return DayNamePickerView()
+    }()
+    
+    private lazy var quantityView: QuantityView = {
+        return QuantityView()
+    }()
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemGray
-        
         configureUI()
+        
+        daysPicker
+            .selectedDayObservable
+            .map { dictionary in
+                dictionary.filter { key, value in
+                    value
+                }
+            }
+            .subscribe(onNext: { print($0) })
+            .disposed(by: disposableBag)
     }
     
     private func configureUI() {
-        view.addSubview(indicateMessage)
+        view.backgroundColor = .white
         
-        indicateMessage.snp.makeConstraints { make in
-            make.center.equalToSuperview()
+        container.addArrangedSubview(titleField)
+        container.addArrangedSubview(startDate)
+        container.addArrangedSubview(endDate)
+        container.addArrangedSubview(daysPicker)
+        container.addArrangedSubview(quantityView)
+        
+        view.addSubview(container)
+        
+        container.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().inset(30)
+            make.width.equalToSuperview().multipliedBy(0.9)
         }
     }
 }
