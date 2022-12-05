9e3c92de639bf585fb1335e74f26ae9b347409ba
jjinwoong <baseball0020@gmail.com>
2022-12-05 21:11:10 +0900

    [feat] Friend view controller 구성

--- DailyQuest/DailyQuest/Presentation/Common/ViewController/FriendViewController.swift
+++ DailyQuest/DailyQuest/Presentation/Common/ViewController/FriendViewController.swift
@@ -7,6 +7,64 @@
 
 import UIKit
 
+import SnapKit
+
 final class FriendViewController: UIViewController {
     
+    // MARK: - Components
+    private lazy var scrollView: UIScrollView = {
+        return UIScrollView()
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        
+        return stackView
+    }()
+    
+    private lazy var friendStatusView: FriendStatusView = {
+        return FriendStatusView()
+    }()
+    
+    /**
+     Calendar view goes here.
+     */
+    
+    /**
+     Quest view goes here.
+     */
+    
+    // MARK: - Life Cycle
+    /**
+     property method, `create` goes here.
+     */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .white
+        stackView.addArrangedSubview(friendStatusView)
+        
+        scrollView.addSubview(stackView)
+        
+        view.addSubview(scrollView)
+        
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+        
+        friendStatusView.snp.makeConstraints { make in
+            make.height.equalTo(100)
+        }
+    }
 }
