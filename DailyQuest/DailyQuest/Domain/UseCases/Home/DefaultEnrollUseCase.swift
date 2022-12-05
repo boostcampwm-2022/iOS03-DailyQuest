8bf4c22585394fffb43306b66a4292a54a007aa5
jjinwoong <baseball0020@gmail.com>
2022-12-05 18:02:24 +0900

    [feat] Enroll use case 구현체 작성

--- DailyQuest/DailyQuest/Domain/UseCases/Home/DefaultEnrollUseCase.swift
+++ DailyQuest/DailyQuest/Domain/UseCases/Home/DefaultEnrollUseCase.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+import RxSwift
+
+final class DefaultEnrollUseCase {
+    private let questsRepository: QuestsRepository
+    
+    init(questsRepository: QuestsRepository) {
+        self.questsRepository = questsRepository
+    }
+}
+
+extension DefaultEnrollUseCase: EnrollUseCase {
+    func save(with quests: [Quest]) -> Observable<Bool> {
+        return questsRepository
+            .save(with: quests)
+            .map { _ in
+                true
+            }
+            .catchAndReturn(false)
+            .asObservable()
+    }
+}
