a6f01c676d711fd55752700f16d2fd81567cd82b
jjinwoong <baseball0020@gmail.com>
2022-12-05 18:21:46 +0900

    [feat] FriendViewController 생성

---
+++ DailyQuest/DailyQuest/Presentation/Common/ViewController/FriendViewController.swift
@@ -0,0 +1,12 @@
+//
+//  FriendViewController.swift
+//  DailyQuest
+//
+//  Created by jinwoong Kim on 2022/12/05.
+//
+
+import UIKit
+
+final class FriendViewController: UIViewController {
+    
+}
