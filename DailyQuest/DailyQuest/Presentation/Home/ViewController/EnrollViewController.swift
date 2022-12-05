e40e374b1f4f4fe7482962c44223d8d0a88ab6b8
jjinwoong <baseball0020@gmail.com>
2022-12-05 17:19:06 +0900

    [feat] EnrollViewController bind메서드 구현

--- DailyQuest/DailyQuest/Presentation/Home/ViewController/EnrollViewController.swift
+++ DailyQuest/DailyQuest/Presentation/Home/ViewController/EnrollViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 import RxSwift
+import RxCocoa
 import SnapKit
 
 final class EnrollViewController: UIViewController {
@@ -51,24 +52,24 @@ final class EnrollViewController: UIViewController {
         return QuantityView()
     }()
     
+    private lazy var submitButton: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.title = "등록하기"
+        config.baseBackgroundColor = .maxYellow
+        config.baseForegroundColor = .maxViolet
+        
+        return UIButton(configuration: config)
+    }()
+    
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        quantityView.quantityField.delegate = self
+        
         configureUI()
         
         bind()
-        
-        viewModel
-            .dayNamePickerViewModel
-            .selectedDayObservable
-            .map { dictionary in
-                dictionary.filter { key, value in
-                    value
-                }
-            }
-            .subscribe(onNext: { print($0) })
-            .disposed(by: disposableBag)
     }
     
     private func configureUI() {
@@ -79,6 +80,7 @@ final class EnrollViewController: UIViewController {
         container.addArrangedSubview(endDate)
         container.addArrangedSubview(daysPicker)
         container.addArrangedSubview(quantityView)
+        container.addArrangedSubview(submitButton)
         
         view.addSubview(container)
         
@@ -91,5 +93,45 @@ final class EnrollViewController: UIViewController {
     
     func bind() {
         daysPicker.bind(with: viewModel.dayNamePickerViewModel, disposeBag: disposableBag)
+        
+        let startDateDidSet = startDate.datePicker.rx
+            .controlEvent(.valueChanged)
+            .withLatestFrom(startDate.datePicker.rx.date)
+        
+        let endDateDidSet = endDate.datePicker.rx
+            .controlEvent(.valueChanged)
+            .withLatestFrom(endDate.datePicker.rx.date)
+        
+        let output = viewModel.transform(
+            input: EnrollViewModel.Input(
+                titleDidChanged: titleField.rx.text.orEmpty.asObservable(),
+                startDateDidSet: startDateDidSet,
+                endDateDidSet: endDateDidSet,
+                quantityDidSet: quantityView.quantityField.rx.text.orEmpty
+                    .asObservable(),
+                submitButtonDidClicked: submitButton.rx.tap.asObservable()
+            )
+        )
+        
+        bindSubmitButton(output: output)
+    }
+    
+    private func bindSubmitButton(output: EnrollViewModel.Output) {
+        output
+            .buttonEnabled
+            .drive(submitButton.rx.isEnabled)
+            .disposed(by: disposableBag)
+    }
+}
+
+extension EnrollViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard CharacterSet(charactersIn: "0123456789").isSuperset(of: CharacterSet(charactersIn: string)) else {
+            return false
+        }
+        guard let oldText = textField.text else { return true }
+        let text = (oldText as NSString).replacingCharacters(in: range, with: string)
+        
+        return text.count < 4
     }
 }
