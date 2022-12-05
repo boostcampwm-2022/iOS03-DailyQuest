d0f7a195a04db4a9598ebbb62e21c8fb5f90a3cb
Dayeon Lee <dayeon2768@daum.net>
2022-12-05 18:35:02 +0900

    [feat] Add ImageView and Button in ProfileVC

--- DailyQuest/DailyQuest/Presentation/Home/ViewController/ProfileViewController.swift
+++ DailyQuest/DailyQuest/Presentation/Home/ViewController/ProfileViewController.swift
@@ -6,10 +6,106 @@
 //
 
 import UIKit
+import SnapKit
+import Kingfisher
 
 class ProfileViewController: UIViewController {
-
+    
+    private lazy var backgroundImage: UIImageView = {
+        let backgroundImage = UIImageView()
+        backgroundImage.image = UIImage(systemName: "photo")
+        backgroundImage.contentMode = .scaleAspectFill
+        backgroundImage.backgroundColor = .red
+        return backgroundImage
+    }()
+    
+    private lazy var userImage: UIImageView = {
+        let userImage = UIImageView()
+        userImage.image = UIImage(named: "AppIcon")
+        userImage.clipsToBounds = true
+        userImage.layer.cornerRadius = 100.0 / 2
+        userImage.contentMode = .scaleAspectFill
+        return userImage
+    }()
+    
+    private lazy var nameLabel: UILabel = {
+        let nameLabel = UILabel()
+        nameLabel.text = "name"
+        nameLabel.textColor = .white
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 15)
+        return nameLabel
+    }()
+    
+    private lazy var introduceLabel: UILabel = {
+        let introduceLabel = UILabel()
+        introduceLabel.text = "한 줄 소개"
+        introduceLabel.textColor = .white
+        introduceLabel.font = UIFont.systemFont(ofSize: 13)
+        return introduceLabel
+    }()
+    
+    private lazy var deleteButton: UIButton = {
+        let deleteButton = UIButton()
+        deleteButton.setTitle("탈퇴하기", for: .normal)
+        deleteButton.setTitleColor(.black, for: .normal)
+        deleteButton.layer.borderWidth = 1
+        deleteButton.layer.cornerRadius = 10
+        return deleteButton
+    }()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.addSubview(backgroundImage)
+        view.addSubview(userImage)
+        view.addSubview(nameLabel)
+        view.addSubview(introduceLabel)
+        view.addSubview(deleteButton)
+        
+        backgroundImage.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.height.equalTo(300)
+        }
+        
+        userImage.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.width.equalTo(100)
+            make.height.equalTo(100)
+            make.top.equalTo(backgroundImage.snp.top).offset(50)
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(userImage.snp.bottom).offset(5)
+        }
+        
+        introduceLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(nameLabel.snp.bottom).offset(5)
+        }
+        
+        deleteButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(backgroundImage.snp.bottom).offset(30)
+            make.height.equalTo(50)
+            make.width.equalTo(300)
+        }
+        
+    }
+    
+}
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct ViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        ProfileViewController().showPreview(.iPhone14Pro)
     }
 }
+#endif
