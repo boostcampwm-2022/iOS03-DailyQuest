f2b0b700226a8f9b96fc97d010712d07f266f753
Dayeon Lee <dayeon2768@daum.net>
2022-12-07 01:50:47 +0900

    [feat] Add ProfileViewModel

---
+++ DailyQuest/DailyQuest/Presentation/Home/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  ProfileViewModel.swift
+//  DailyQuest
+//
+//  Created by 이다연 on 2022/12/06.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class ProfileViewModel {
+    
+    private let userUseCase: UserUseCase
+    
+    private var disposableBag = DisposeBag()
+    
+    init(userUseCase: UserUseCase) {
+        self.userUseCase = userUseCase
+    }
+    
+    struct Input {
+        let viewDidLoad: Observable<Void>
+        let deleteUserButtonDidClicked: Observable<Void>
+    }
+    
+    struct Output {
+        let data: Driver<User>
+        let deleteUserResult: Observable<Bool>
+    }
+    
+    func transform(input: Input) -> Output {
+        let data = input
+            .viewDidLoad
+            .flatMap { _ in
+                self.userUseCase.fetch()
+            }
+            .asDriver(onErrorJustReturn: User())
+        
+        let deleteUserResult = input.deleteUserButtonDidClicked.flatMap { _ in
+            self.userUseCase.delete()
+        }
+        return Output(data: data, deleteUserResult: deleteUserResult)
+    }
+}
