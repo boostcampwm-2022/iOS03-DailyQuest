3d61ad6ada0e3b6b1ba516fdda128953daf5451b
jjinwoong <baseball0020@gmail.com>
2022-12-06 12:03:34 +0900

    [fix] 네트워킹 부분 수정

--- DailyQuest/DailyQuest/Data/Repositories/DefaultQuestsRepository.swift
+++ DailyQuest/DailyQuest/Data/Repositories/DefaultQuestsRepository.swift
@@ -9,10 +9,10 @@ import RxSwift
 import Foundation
 
 final class DefaultQuestsRepository {
-
+    
     private let persistentStorage: QuestsStorage
     private let networkService: NetworkService
-
+    
     init(persistentStorage: QuestsStorage, networkService: NetworkService = FirebaseService.shared) {
         self.persistentStorage = persistentStorage
         self.networkService = networkService
@@ -23,68 +23,71 @@ extension DefaultQuestsRepository: QuestsRepository {
     func save(with quest: [Quest]) -> Single<[Quest]> {
         return persistentStorage.saveQuests(with: quest)
             .flatMap { quests in
-            return Observable.from(quests)
-                .concatMap { quest in
-                return self.networkService.create(userCase: .currentUser,
-                                                  access: .quests,
-                                                  dto: quest.toDTO())
+                return Observable.from(quests)
+                    .withUnretained(self)
+                    .concatMap { (owner, quest) in
+                        return owner.networkService.create(userCase: .currentUser,
+                                                          access: .quests,
+                                                          dto: quest.toDTO())
+                        .map { $0.toDomain() }
+                        .catchAndReturn(quest)
+                    }
+                    .toArray()
             }
-                .map { $0.toDomain() }
-                .toArray()
-        }
     }
-
+    
     func fetch(by date: Date) -> Observable<[Quest]> {
         return persistentStorage.fetchQuests(by: date)
             .catch { _ in
-            self.networkService.read(type: QuestDTO.self, userCase: .currentUser, access: .quests, filter: .today(date))
-                .map { $0.toDomain() }
-                .toArray()
-                .asObservable()
-        }
+                self.networkService.read(type: QuestDTO.self, userCase: .currentUser, access: .quests, filter: .today(date))
+                    .map { $0.toDomain() }
+                    .toArray()
+                    .asObservable()
+            }
     }
-
+    
     func update(with quest: Quest) -> Single<Quest> {
         return persistentStorage.updateQuest(with: quest)
-            .flatMap { quest in
-            self.networkService.update(userCase: .currentUser, access: .quests, dto: quest.toDTO())
-        }
-            .map { $0.toDomain() }
-            .asObservable()
-            .asSingle()
+            .flatMap(updateNetworkService(quest:))
     }
-
+    
+    private func updateNetworkService(quest: Quest) -> Single<Quest> {
+        return networkService.update(userCase: .currentUser, access: .quests, dto: quest.toDTO())
+            .map { $0.toDomain() }
+            .catchAndReturn(quest)
+    }
+    
     func delete(with questId: UUID) -> Single<Quest> {
         return persistentStorage.deleteQuest(with: questId)
             .flatMap { quest in
-            self.networkService.delete(userCase: .currentUser, access: .quests, dto: quest.toDTO())
-        }
+                self.networkService.delete(userCase: .currentUser, access: .quests, dto: quest.toDTO())
+            }
             .map { $0.toDomain() }
             .asObservable()
             .asSingle()
     }
-
+    
     func deleteAll(with groupId: UUID) -> Single<[Quest]> {
         return persistentStorage.deleteQuestGroup(with: groupId)
             .flatMap { quests in
-            return Observable.from(quests)
-                .concatMap { quest in
-                self.networkService.delete(userCase: .currentUser,
-                                           access: .quests,
-                                           dto: quest.toDTO())
+                return Observable.from(quests)
+                    .concatMap { quest in
+                        self.networkService.delete(userCase: .currentUser,
+                                                   access: .quests,
+                                                   dto: quest.toDTO())
+                    }
+                    .map { $0.toDomain() }
+                    .toArray()
             }
-                .map { $0.toDomain() }
-                .toArray()
-        }
     }
-
+    
     func fetch(by uuid: String, date: Date) -> Observable<[Quest]> { // 받을 날짜까지 받아와야함
         return self.networkService.read(type: QuestDTO.self,
                                         userCase: .anotherUser(uuid),
                                         access: .quests,
                                         filter: .today(date))
-            .map { $0.toDomain() }
-            .toArray()
-            .asObservable()
+        .map { $0.toDomain() }
+        .toArray()
+        .asObservable()
     }
 }
