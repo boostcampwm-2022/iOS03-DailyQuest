7216d081cbc20385aea0dfd7fe994fe772836416
jjinwoong <baseball0020@gmail.com>
2022-12-06 14:00:18 +0900

    [feat] SettingsUseCase 구현체 작성

--- DailyQuest/DailyQuest/Domain/UseCases/Settings/DefaultSettingsUseCase.swift
+++ DailyQuest/DailyQuest/Domain/UseCases/Settings/DefaultSettingsUseCase.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+import RxSwift
+
+final class DefaultSettingsUseCase {
+    private let userRepository: UserRepository
+    private let authRepository: AuthRepository
+    
+    init(userRepository: UserRepository, authRepository: AuthRepository) {
+        self.userRepository = userRepository
+        self.authRepository = authRepository
+    }
+}
+
+extension DefaultSettingsUseCase: SettingsUseCase {
+    func isLoggedIn() -> Observable<Bool> {
+        return userRepository
+            .isLoggedIn()
+            .map { id in
+                guard let _ = id else { return false }
+                return true
+            }
+            .asObservable()
+    }
+    
+    func signOut() -> Observable<Bool> {
+        return authRepository
+            .signOut()
+            .map { _ in true }
+            .catchAndReturn(false)
+            .asObservable()
+    }
+}
