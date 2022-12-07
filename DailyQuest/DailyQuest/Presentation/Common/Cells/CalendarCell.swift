31f6c9a0b1d0a01695b9484efdb7d54a73377c42
jjinwoong <baseball0020@gmail.com>
2022-12-07 17:16:55 +0900

    [refactor] CalendarCell이 날짜의 일을 표기하게 변경

--- DailyQuest/DailyQuest/Presentation/Common/Cells/CalendarCell.swift
+++ DailyQuest/DailyQuest/Presentation/Common/Cells/CalendarCell.swift
@@ -78,6 +78,6 @@ final class CalendarCell: UICollectionViewCell {
         case .done:
             self.circleCheckView.setDone()
         }
-        dayLabel.text = "\(completion.day)"
+        dayLabel.text = "\(completion.day.day)"
     }
 }
