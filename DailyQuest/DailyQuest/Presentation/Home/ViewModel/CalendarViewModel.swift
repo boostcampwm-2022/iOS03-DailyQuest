fe8f3439db0618b067d97202bb47def452888cc5
wickedRun <hoon3284@icloud.com>
2022-12-07 13:55:58 +0900

    [feat] CalendarViewModel 구현

--- DailyQuest/DailyQuest/Presentation/Home/ViewModel/CalendarViewModel.swift
+++ DailyQuest/DailyQuest/Presentation/Home/ViewModel/CalendarViewModel.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+
+import RxSwift
+import RxRelay
+
+final class CalendarViewModel {
+    
+    private var calendarUseCase: CalendarUseCase
+
+    init(calendarUseCase: CalendarUseCase) {
+        self.calendarUseCase = calendarUseCase
+    }
+    
+    struct Input {
+        let viewDidLoad: Observable<Void>
+        let dragEventInCalendar: Observable<Bool>
+    }
+    
+    struct Output {
+        let currentMonth = BehaviorRelay<Date?>(value: Date())
+        let displayDays = BehaviorRelay<[[DailyQuestCompletion]]>(value: [[], [], []])
+    }
+    
+    func transform(input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+        
+        input.viewDidLoad
+            .subscribe(onNext: { [weak self] in
+                self?.calendarUseCase.setupMonths()
+            })
+            .disposed(by: disposeBag)
+        
+        input.dragEventInCalendar
+            .subscribe(onNext: { [weak self] direction in
+                if direction {
+                    self?.calendarUseCase.fetchNextMontlyCompletion()
+                } else {
+                    self?.calendarUseCase.fetchLastMontlyCompletion()
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        calendarUseCase.completionOfMonths
+            .subscribe(onNext: { monthlyCompletion in
+                output.displayDays.accept(monthlyCompletion)
+            })
+            .disposed(by: disposeBag)
+        
+        calendarUseCase.currentMonth
+            .subscribe(onNext: { month in
+                output.currentMonth.accept(month)
+            })
+            .disposed(by: disposeBag)
+        
+        return output
+    }
+}
