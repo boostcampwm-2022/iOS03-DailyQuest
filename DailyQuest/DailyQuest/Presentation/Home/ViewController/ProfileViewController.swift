afb2182e1a6854b715cdbf33f9e9ad9a5adea1f6
Dayeon Lee <dayeon2768@daum.net>
2022-12-07 18:06:03 +0900

    [feat] Add alert

--- DailyQuest/DailyQuest/Presentation/Home/ViewController/ProfileViewController.swift
+++ DailyQuest/DailyQuest/Presentation/Home/ViewController/ProfileViewController.swift
@@ -11,17 +11,19 @@ import RxSwift
 import RxCocoa
 
 final class ProfileViewController: UIViewController {
-
+    
     private var viewModel: ProfileViewModel!
     private var disposableBag = DisposeBag()
-
+    
+    private var deleteUserButtonDidClicked = PublishSubject<Event>()
+    
     private lazy var backgroundImage: UIImageView = {
         let backgroundImage = UIImageView()
         backgroundImage.image = UIImage(named: "defaultBackground")
         backgroundImage.contentMode = .scaleToFill
         return backgroundImage
     }()
-
+    
     private lazy var userImage: UIImageView = {
         let userImage = UIImageView()
         userImage.image = UIImage(named: "AppIcon")
@@ -30,7 +32,7 @@ final class ProfileViewController: UIViewController {
         userImage.contentMode = .scaleAspectFill
         return userImage
     }()
-
+    
     private lazy var nameLabel: UILabel = {
         let nameLabel = UILabel()
         nameLabel.text = "name"
@@ -38,7 +40,7 @@ final class ProfileViewController: UIViewController {
         nameLabel.font = UIFont.boldSystemFont(ofSize: 15)
         return nameLabel
     }()
-
+    
     private lazy var introduceLabel: UILabel = {
         let introduceLabel = UILabel()
         introduceLabel.text = "한 줄 소개"
@@ -46,7 +48,7 @@ final class ProfileViewController: UIViewController {
         introduceLabel.font = UIFont.systemFont(ofSize: 13)
         return introduceLabel
     }()
-
+    
     private lazy var deleteUserButton: UIButton = {
         let deleteButton = UIButton()
         deleteButton.setTitle("탈퇴하기", for: .normal)
@@ -55,51 +57,51 @@ final class ProfileViewController: UIViewController {
         deleteButton.layer.cornerRadius = 10
         return deleteButton
     }()
-
+    
     // MARK: - Life Cycle
     static func create(with viewModel: ProfileViewModel) -> ProfileViewController {
         let vc = ProfileViewController()
         vc.viewModel = viewModel
         return vc
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
         bind()
     }
-
+    
     private func configureUI() {
         view.backgroundColor = .white
-
+        
         view.addSubview(backgroundImage)
         view.addSubview(userImage)
         view.addSubview(nameLabel)
         view.addSubview(introduceLabel)
         view.addSubview(deleteUserButton)
-
+        
         backgroundImage.snp.makeConstraints { make in
             make.width.equalToSuperview()
             make.height.equalTo(300)
         }
-
+        
         userImage.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.width.equalTo(100)
             make.height.equalTo(100)
             make.top.equalTo(backgroundImage.snp.bottom).offset(-50)
         }
-
+        
         nameLabel.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(userImage.snp.bottom).offset(5)
         }
-
+        
         introduceLabel.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(nameLabel.snp.bottom).offset(5)
         }
-
+        
         deleteUserButton.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(introduceLabel.snp.bottom).offset(10)
@@ -107,30 +109,81 @@ final class ProfileViewController: UIViewController {
             make.width.equalTo(300)
         }
     }
-
+    
     func bind() {
-        let input = ProfileViewModel.Input(viewDidLoad: .just(()).asObservable(), deleteUserButtonDidClicked: deleteUserButton.rx.tap.asObservable())
+        deleteUserButton.rx.tap//.asDriver().drive(onDisposed:  {
+            .subscribe(onNext: {
+            print("엄")
+            self.showAlert()
+        }).disposed(by: disposableBag)
+        let input = ProfileViewModel.Input(viewDidLoad: .just(()).asObservable(), deleteUserButtonDidClicked: deleteUserButtonDidClicked)
         let output = viewModel.transform(input: input)
-
+        
         output
             .data
             .drive(onNext: { user in
-                print(user)
-            self.userImage.setImage(with: user.profileURL)
-            self.backgroundImage.setImage(with: user.backgroundImageURL)
-            self.nameLabel.text = user.nickName
-            self.introduceLabel.text = user.introduce
-        })
+                self.userImage.setImage(with: user.profileURL)
+                self.backgroundImage.setImage(with: user.backgroundImageURL)
+                self.nameLabel.text = user.nickName
+                self.introduceLabel.text = user.introduce
+            })
+            .disposed(by: disposableBag)
+        output
+            .deleteUserResult
+            .drive(onNext: { bool in
+                if bool {
+                    let message = "탈퇴가 완료되었습니다."
+                    let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+                    let okAction = UIAlertAction(title: "OK", style: .cancel) { (action) in
+                        self.dismiss(animated: true)
+                    }
+                    alert.addAction(okAction)
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    let message = "오류가 발생했습니다./n잠시 후 다시 시도해주세요."
+                    let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+                    let okAction = UIAlertAction(title: "OK", style: .cancel)
+                    alert.addAction(okAction)
+                    self.present(alert, animated: true, completion: nil)
+                }
+            })
             .disposed(by: disposableBag)
     }
 }
 
-#if canImport(SwiftUI) && DEBUG
-    import SwiftUI
-
-    struct ViewController_Preview: PreviewProvider {
-        static var previews: some View {
-            ProfileViewController().showPreview(.iPhone14Pro)
+extension ProfileViewController {
+    func showAlert(preferredStyle: UIAlertController.Style = .alert,
+                   completion: (() -> Void)? = nil)
+    {
+        let title = "탈퇴하기"
+        let message = "정말 탈퇴 하시겠습니까?"
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { [weak self] _ in
+            self?.deleteUserButtonDidClicked.onNext(.deleteUser)
         }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true, completion: completion)
     }
+}
+
+extension ProfileViewController {
+    enum Event {
+        case deleteUser
+    }
+}
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct ViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        ProfileViewController().showPreview(.iPhone14Pro)
+    }
+}
 #endif
