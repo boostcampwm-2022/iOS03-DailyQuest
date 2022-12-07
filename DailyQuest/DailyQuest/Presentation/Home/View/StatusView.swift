06a0c241b788d603435aab0009bfde159927f5a6
Jeonhui <dlwjsgml02@naver.com>
2022-12-08 00:52:57 +0900

    [feat] message bubble 수정

--- DailyQuest/DailyQuest/Presentation/Home/View/StatusView.swift
+++ DailyQuest/DailyQuest/Presentation/Home/View/StatusView.swift
@@ -15,16 +15,19 @@ final class StatusView: UIView {
     var profileButtonDidClick = PublishSubject<Void>()
     var userDataFetched = PublishSubject<User>()
 
-    // MARK: - Components
-    private lazy var iconContainer: UIImageView = {
-        let iconContainer = UIImageView()
-        iconContainer.image = UIImage(named: "StatusMax")
+    private let messages = ["화이팅", "잘 할 수 있어", "오늘은 공부를 해보자!", "Hello, World!", "🎹🎵🎶🎵🎶"]
 
+    // MARK: - Components
+    private lazy var iconContainer: UIButton = {
+        var config = UIButton.Configuration.plain()
+        config.image = UIImage(named: "StatusMax")
+        let iconContainer = UIButton(configuration: config)
+        iconContainer.imageView?.contentMode = .scaleToFill
         return iconContainer
     }()
 
     private lazy var messageBubble: MessageBubbleLabel = {
-        return MessageBubbleLabel()
+        return MessageBubbleLabel(text: getRandomMessage())
     }()
 
     private lazy var statusLabel: UILabel = {
@@ -49,11 +52,9 @@ final class StatusView: UIView {
         let largeConfig = UIImage.SymbolConfiguration(pointSize: 40, weight: .bold, scale: .large)
         var config = UIButton.Configuration.plain()
         config.baseForegroundColor = .maxLightGrey
+        config.image = UIImage(systemName: "person.crop.circle",
+                               withConfiguration: largeConfig)
         let button = UIButton(configuration: config)
-        button.setImage(UIImage(systemName: "person.crop.circle",
-                                withConfiguration: largeConfig),
-                        for: .normal)
-        button.layer.cornerRadius = 100
         return button
     }()
 
@@ -75,6 +76,10 @@ final class StatusView: UIView {
             make.height.equalToSuperview()
             make.width.equalTo(iconContainer.snp.height)
             make.top.leading.bottom.equalToSuperview()
+        }
+
+        iconContainer.imageView?.snp.makeConstraints { make in
+            make.width.height.equalToSuperview()
         }
 
         addSubview(messageBubble)
@@ -105,6 +110,13 @@ final class StatusView: UIView {
     }
 
     private func bind() {
+        iconContainer.rx.tap
+            .subscribe(onNext: { [weak self] _ in
+            guard let self = self else { return }
+            self.messageBubble.setText(text: self.getRandomMessage())
+        })
+            .disposed(by: disposableBag)
+
         profileButton.rx.tap
             .subscribe(onNext: { [weak self] _ in
             self?.profileButtonDidClick.onNext(())
@@ -117,5 +129,13 @@ final class StatusView: UIView {
             guard let self = self else { return }
             self.profileButton.imageView?.setImage(with: user.profileURL)
         }).disposed(by: disposableBag)
+
+
+    }
+}
+
+extension StatusView {
+    private func getRandomMessage() -> String {
+        return messages.randomElement() ?? "Hello,World!"
     }
 }
