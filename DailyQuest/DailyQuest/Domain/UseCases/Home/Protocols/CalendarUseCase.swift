a48dc331fe58382bcae47e58bed89c1f8fba57c1
wickedRun <hoon3284@icloud.com>
2022-12-08 14:17:03 +0900

    [feat] CalendarUseCase Protocol에 필요한 변수, 함수 추가

--- DailyQuest/DailyQuest/Domain/UseCases/Home/Protocols/CalendarUseCase.swift
+++ DailyQuest/DailyQuest/Domain/UseCases/Home/Protocols/CalendarUseCase.swift
@@ -13,8 +13,12 @@ protocol CalendarUseCase {
     
     var currentMonth: BehaviorSubject<Date?> { get }
     var completionOfMonths: BehaviorSubject<[[DailyQuestCompletion]]> { get }
+    var selectedDate: BehaviorSubject<Date> { get }
+    var selectedDateCompletion: BehaviorSubject<DailyQuestCompletion?> { get }
     
     func fetchNextMontlyCompletion()
     func fetchLastMontlyCompletion()
     func setupMonths()
+    func refreshMontlyCompletion(for date: Date)
+    func selectDate(_ date: Date)
 }
