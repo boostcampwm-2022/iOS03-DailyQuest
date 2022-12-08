9c26019a207d72a1603cdae8c1f028d0ebf41930
jjinwoong <baseball0020@gmail.com>
2022-12-08 23:23:18 +0900

    [feat] FriendCalendarUseCase 추가

--- DailyQuest/DailyQuest/Domain/UseCases/Common/Protocols/DefaultFriendCalendarUseCase.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DefaultFriendCalendarUseCase.swift
-//  DailyQuest
-//
-//  Created by jinwoong Kim on 2022/12/08.
-//
-
-import Foundation
--- DailyQuest/DailyQuest/Domain/UseCases/Common/Protocols/FriendCalendarUseCase.swift
+++ DailyQuest/DailyQuest/Domain/UseCases/Common/Protocols/FriendCalendarUseCase.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+import RxSwift
+
+protocol FriendCalendarUseCase {
+    var currentMonth: BehaviorSubject<Date?> { get }
+    var completionOfMonths: BehaviorSubject<[[DailyQuestCompletion]]> { get }
+    var selectedDate: BehaviorSubject<Date> { get }
+    
+    func fetchNextMontlyCompletion()
+    func fetchLastMontlyCompletion()
+    func setupMonths()
+    func refreshMontlyCompletion(for date: Date)
+    func selectDate(_ date: Date)
+}
