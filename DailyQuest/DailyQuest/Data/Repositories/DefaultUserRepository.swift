d2490d5fdb207f88d9631eb6c0f6b2e64d35ea72
Jeonhui <dlwjsgml02@naver.com>
2022-12-08 23:19:12 +0900

    [feat] if change user Iamge { notify }

--- DailyQuest/DailyQuest/Data/Repositories/DefaultUserRepository.swift
+++ DailyQuest/DailyQuest/Data/Repositories/DefaultUserRepository.swift
@@ -10,12 +10,12 @@ import RxSwift
 import RxRelay
 
 final class DefaultUserRepository {
-
+    
     private let persistentStorage: UserInfoStorage
     private let networkService: NetworkService
-
+    
     private let disposeBag = DisposeBag()
-
+    
     init(persistentStorage: UserInfoStorage, networkService: NetworkService = FirebaseService.shared) {
         self.persistentStorage = persistentStorage
         self.networkService = networkService
@@ -26,59 +26,64 @@ extension DefaultUserRepository: UserRepository {
     func isLoggedIn() -> BehaviorRelay<String?> {
         return networkService.uid
     }
-
+    
     func readUser() -> Single<User> {
         return self.persistentStorage.fetchUserInfo()
             .catch { [weak self] _ in
-            guard let self = self else { return Observable.just(User()) }
-            return self.fetchUserNetworkService()
-        }
+                guard let self = self else { return Observable.just(User()) }
+                return self.fetchUserNetworkService()
+            }
             .asSingle()
     }
-
+    
     func updateUser(by user: User) -> Single<User> {
         return persistentStorage.updateUserInfo(user: user)
             .asObservable()
             .flatMap(updateUserNetworkService(user:))
             .asSingle()
     }
-
+    
     func fetchUser(by uuid: String) -> Single<User> {
         return networkService.read(type: UserDTO.self,
                                    userCase: .anotherUser(uuid),
                                    access: .userInfo,
                                    filter: nil)
-            .map { $0.toDomain() }
-            .asSingle()
+        .map { $0.toDomain() }
+        .asSingle()
     }
-
+    
     func saveProfileImage(data: Data) -> Single<Bool> {
         networkService.uploadDataStorage(data: data, path: .profileImages)
             .flatMap { [weak self] downloadUrl in
-            guard let self = self else { return Single.just(User()) }
-            return self.persistentStorage.fetchUserInfo()
-                .map { $0.setProfileImageURL(profileURL: downloadUrl) }
-                .asSingle()
-        }
+                guard let self = self else { return Single.just(User()) }
+                return self.persistentStorage.fetchUserInfo()
+                    .map { $0.setProfileImageURL(profileURL: downloadUrl) }
+                    .asSingle()
+            }
             .flatMap(updateUser(by:))
             .map { _ in true }
             .catchAndReturn(false)
+            .do(afterSuccess: { _ in
+                NotificationCenter.default.post(name: .userUpdated, object: nil)
+            })
     }
-
+    
     func saveBackgroundImage(data: Data) -> Single<Bool> {
         networkService.uploadDataStorage(data: data, path: .backgroundImages)
             .flatMap { [weak self] downloadUrl in
-            guard let self = self else { return Single.just(User()) }
-            return self.persistentStorage.fetchUserInfo()
-                .map { $0.setBackgroundImageURL(backgroundImageURL: downloadUrl) }
-                .asSingle()
-        }
+                guard let self = self else { return Single.just(User()) }
+                return self.persistentStorage.fetchUserInfo()
+                    .map { $0.setBackgroundImageURL(backgroundImageURL: downloadUrl) }
+                    .asSingle()
+            }
             .flatMap(updateUser(by:))
             .map { _ in true }
             .catchAndReturn(false)
-
+            .do(afterSuccess: { _ in
+                NotificationCenter.default.post(name: .userUpdated, object: nil)
+            })
     }
-
+    
 }
 
 extension DefaultUserRepository: ProtectedUserRepository {
@@ -86,17 +91,16 @@ extension DefaultUserRepository: ProtectedUserRepository {
         return networkService
             .delete(userCase: .currentUser, access: .userInfo, dto: UserDTO())
             .flatMap { [weak self] _ in
-            guard let self = self else { return .just(true) }
-            return self.networkService.deleteUser() }
+                guard let self = self else { return .just(true) }
+                return self.networkService.deleteUser() }
             .map { _ in true }
             .catchAndReturn(false)
             .asObservable()
             .do(onNext: { [weak self]_ in
-            guard let self = self else { return }
-            self.networkService.signOut().subscribe()
-                .disposed(by: self.disposeBag)
-        })
-
+                guard let self = self else { return }
+                self.networkService.signOut().subscribe()
+                    .disposed(by: self.disposeBag)
+            })
     }
 }
 
@@ -105,7 +109,7 @@ private extension DefaultUserRepository {
         networkService.read(type: UserDTO.self, userCase: .currentUser, access: .userInfo, filter: nil)
             .map { $0.toDomain() }
     }
-
+    
     func updateUserNetworkService(user: User) -> Observable<User> {
         networkService.update(userCase: .currentUser, access: .userInfo, dto: user.toDTO())
             .map { $0.toDomain() }
