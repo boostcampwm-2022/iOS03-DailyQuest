76e7e4965d76c76b49ef855905a745436d71d59e
Dayeon Lee <dayeon2768@daum.net>
2022-12-11 19:29:00 +0900

    [feat] Delete deleteUserButton in ProfileView

--- DailyQuest/DailyQuest/Presentation/Home/ViewController/ProfileViewController.swift
+++ DailyQuest/DailyQuest/Presentation/Home/ViewController/ProfileViewController.swift
@@ -16,8 +16,7 @@ final class ProfileViewController: UIViewController {
     
     private var viewModel: ProfileViewModel!
     private var disposableBag = DisposeBag()
-    
-    private let deleteUserButtonDidClicked = PublishSubject<Event>()
+
     private let changeProfileImage = PublishSubject<UIImage?>()
     
     private lazy var imagePicker: PHPickerViewController = {
@@ -55,15 +54,7 @@ final class ProfileViewController: UIViewController {
         editIntroduceButton.tintColor = .gray
         return editIntroduceButton
     }()
-    
-    private lazy var deleteUserButton: UIButton = {
-        let deleteButton = UIButton()
-        deleteButton.setTitle("탈퇴하기", for: .normal)
-        deleteButton.setTitleColor(.maxViolet, for: .normal)
-        deleteButton.backgroundColor = .maxYellow
-        deleteButton.layer.cornerRadius = 10
-        return deleteButton
-    }()
+
     
     // MARK: - Life Cycle
     static func create(with viewModel: ProfileViewModel) -> ProfileViewController {
@@ -85,7 +76,6 @@ final class ProfileViewController: UIViewController {
         view.addSubview(nameLabel)
         view.addSubview(introduceLabel)
         view.addSubview(editIntroduceButton)
-        view.addSubview(deleteUserButton)
         
         userImageView.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
@@ -107,13 +97,7 @@ final class ProfileViewController: UIViewController {
             make.top.equalTo(nameLabel.snp.bottom).offset(5)
             make.left.equalTo(introduceLabel.snp.right).offset(5)
         }
-        
-        deleteUserButton.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(editIntroduceButton.snp.bottom).offset(10)
-            make.height.equalTo(50)
-            make.width.equalTo(300)
-        }
+
     }
     
     func bind() {
@@ -141,12 +125,7 @@ final class ProfileViewController: UIViewController {
             }
             )
         
-        deleteUserButton.rx.tap
-            .subscribe(onNext: {
-                self.showAlert()
-            }).disposed(by: disposableBag)
-        
-        let input = ProfileViewModel.Input(viewDidLoad: .just(()).asObservable(), deleteUserButtonDidClicked: deleteUserButtonDidClicked, changeProfileImage: changeProfileImage)
+        let input = ProfileViewModel.Input(viewDidLoad: .just(()).asObservable(),changeProfileImage: changeProfileImage)
         let output = viewModel.transform(input: input)
         
         output
@@ -155,26 +134,6 @@ final class ProfileViewController: UIViewController {
                 self.userImageView.userImage.setImage(with: user.profileURL)
                 self.nameLabel.text = user.nickName
                 self.introduceLabel.text = user.introduce
-            })
-            .disposed(by: disposableBag)
-        output
-            .deleteUserResult
-            .drive(onNext: { bool in
-                if bool {
-                    let message = "탈퇴가 완료되었습니다."
-                    let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-                    let okAction = UIAlertAction(title: "OK", style: .cancel) { (action) in
-                        self.dismiss(animated: true)
-                    }
-                    alert.addAction(okAction)
-                    self.present(alert, animated: true, completion: nil)
-                } else {
-                    let message = "오류가 발생했습니다. \n 잠시 후 다시 시도해주세요."
-                    let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-                    let okAction = UIAlertAction(title: "OK", style: .cancel)
-                    alert.addAction(okAction)
-                    self.present(alert, animated: true, completion: nil)
-                }
             })
             .disposed(by: disposableBag)
         
@@ -188,33 +147,6 @@ final class ProfileViewController: UIViewController {
                 }
             })
             .disposed(by: disposableBag)
-    }
-}
-
-extension ProfileViewController {
-    func showAlert(preferredStyle: UIAlertController.Style = .alert,
-                   completion: (() -> Void)? = nil)
-    {
-        let title = "탈퇴하기"
-        let message = "정말 탈퇴 하시겠습니까?"
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
-        let okAction = UIAlertAction(title: "OK", style: .default) { [weak self] _ in
-            self?.deleteUserButtonDidClicked.onNext(.deleteUser)
-        }
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
-        
-        alert.addAction(okAction)
-        alert.addAction(cancelAction)
-        
-        self.present(alert, animated: true, completion: completion)
-    }
-}
-
-extension ProfileViewController {
-    enum Event {
-        case deleteUser
     }
 }
 
