83d6fe2fce7f5027695629e7d5b4dba82daf72c2
jjinwoong <baseball0020@gmail.com>
2022-12-12 20:42:18 +0900

    [feat] HomeScene에 사용되는 usecase key 생성

--- DailyQuest/DailyQuest/Application/Container/UseCase/UseCaseKey.swift
+++ DailyQuest/DailyQuest/Application/Container/UseCase/UseCaseKey.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import DailyContainer
+
+// MARK: - Home Scene
+struct QuestUseCaseKey: InjectionKey {
+    typealias Value = QuestUseCase
+}
+
+struct EnrollUseCaseKey: InjectionKey {
+    typealias Value = EnrollUseCase
+}
+
+struct UserUseCaseKey: InjectionKey {
+    typealias Value = UserUseCase
+}
+
+struct CalendarUseCaseKey: InjectionKey {
+    typealias Value = CalendarUseCase
+}
+
+// MARK: - Browse Scene
+
+// MARK: - Settings Scene
