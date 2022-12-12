cd5b5c6d5c489efe55221189dc0c254054c3ea1c
jjinwoong <baseball0020@gmail.com>
2022-12-12 21:31:57 +0900

    [feat] CustomFramework, DailyContainer 추가

---
+++ DailyQuest/SubFrameworks/DailyContainer/DailyContainer/Container.swift
@@ -0,0 +1,55 @@
+//
+//  Container.swift
+//  DailyContainer
+//
+//  Created by jinwoong Kim on 2022/12/12.
+//
+
+import Foundation
+
+public class Container {
+    public static var shared = Container()
+    
+    private var modules: [String: Module] = [:]
+    
+    public init() {}
+    deinit { modules.removeAll() }
+}
+
+extension Container {
+    func register(with module: Module) {
+        modules[module.name] = module
+    }
+    
+    func resolve<T>(for type: Any.Type?) -> T {
+        let name = type.map { String(describing: $0) } ?? String(describing: T.self)
+        
+        guard let dependancy: T = modules[name]?.resolve() as? T else {
+            fatalError("dependancy \(T.self) not resolved.")
+        }
+        
+        return dependancy
+    }
+}
+
+public extension Container {
+    func register(@ModuleBuilder _ modules: () -> [Module]) {
+        modules().forEach { register(with: $0) }
+    }
+    
+    func register(@ModuleBuilder _ module: () -> Module) {
+        register(with: module())
+    }
+    
+    @resultBuilder
+    struct ModuleBuilder {
+        public static func buildBlock(_ modules: Module...) -> [Module] {
+            modules
+        }
+        
+        public static func buildBlock(_ module: Module) -> Module {
+            module
+        }
+    }
+}
+
