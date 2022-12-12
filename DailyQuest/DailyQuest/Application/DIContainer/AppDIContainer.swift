87639a36efa7c19770adc487346cfc53a26b4df9
jjinwoong <baseball0020@gmail.com>
2022-12-12 20:42:42 +0900

    [feat] AppDIContainer에서 Service와 Repository를 regist

--- DailyQuest/DailyQuest/Application/DIContainer/AppDIContainer.swift
+++ DailyQuest/DailyQuest/Application/DIContainer/AppDIContainer.swift
@@ -6,8 +6,15 @@
 //
 
 import Foundation
+import DailyContainer
 
 final class AppDIContainer {
+    
+    init() {
+        registService()
+        registStorage()
+        registRepository()
+    }
     
     func makeHomeSceneDIContainer() -> HomeSceneDIContainer {
         return HomeSceneDIContainer()
@@ -20,4 +27,68 @@ final class AppDIContainer {
     func makeSettingsSceneDIContainer() -> SettingsSceneDIContainer {
         return SettingsSceneDIContainer()
     }
+}
+
+private extension AppDIContainer {
+    func registService() {
+        Container.shared.regist {
+            Module(ServiceKey.self) { FirebaseService.shared }
+        }
+    }
+    
+    func registStorage() {
+        Container.shared.regist {
+            Module(QuestStorageKey.self) { RealmQuestsStorage() }
+            Module(BrowseQuestStorageKey.self) { RealmBrowseQuestsStorage() }
+            Module(UserInfoStorageKey.self) { RealmUserInfoStorage() }
+        }
+    }
+    
+    func registRepository() {
+        Container.shared.regist {
+            Module(QuestRepositoryKey.self) {
+                @Injected(QuestStorageKey.self)
+                var questStorage: QuestsStorage
+                return DefaultQuestsRepository(persistentStorage: questStorage)
+            }
+            
+            Module(AuthRepositoryKey.self) {
+                @Injected(QuestStorageKey.self)
+                var questStorage: QuestsStorage
+                
+                @Injected(UserInfoStorageKey.self)
+                var userInfoStorage: UserInfoStorage
+                
+                return DefaultAuthRepository(persistentQuestsStorage: questStorage,
+                                             persistentUserStorage: userInfoStorage)
+            }
+            
+            Module(BrowseRepositoryKey.self) {
+                @Injected(BrowseQuestStorageKey.self)
+                var browseQuestStroage: BrowseQuestsStorage
+                
+                @Injected(ServiceKey.self)
+                var networkService: NetworkService
+                
+                return DefaultBrowseRepository(persistentStorage: browseQuestStroage,
+                                               networkService: networkService)
+            }
+            
+            /**
+             Networ service instance needed.
+             */
+            Module(UserRepositoryKey.self) {
+                @Injected(UserInfoStorageKey.self)
+                var userInfoStorage: UserInfoStorage
+                
+                return DefaultUserRepository(persistentStorage: userInfoStorage)
+            }
+            
+            /**
+             Protected User Repository Injection
+             goes here.
+             */
+        }
+    }
+
 }
