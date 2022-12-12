6a0a8c9bb3206ce5a80c4f09ec5fe09922ba7b73
jjinwoong <baseball0020@gmail.com>
2022-12-12 20:42:02 +0900

    [feat] Repository Key 생성

--- DailyQuest/DailyQuest/Application/Container/Repository/RepositoryKey.swift
+++ DailyQuest/DailyQuest/Application/Container/Repository/RepositoryKey.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import DailyContainer
+
+struct QuestRepositoryKey: InjectionKey {
+    typealias Value = QuestsRepository
+}
+
+struct AuthRepositoryKey: InjectionKey {
+    typealias Value = AuthRepository
+}
+
+struct BrowseRepositoryKey: InjectionKey {
+    typealias Value = BrowseRepository
+}
+
+struct UserRepositoryKey: InjectionKey {
+    typealias Value = UserRepository
+}
+
+struct ProtectedUserRepositoryKey: InjectionKey {
+    typealias Value = ProtectedUserRepository
+}
